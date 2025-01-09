80e008a99528070bb31f658d323d6f91abab22ec
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2025-01-09 14:53:12 +0530

    added

--- src/app.js
+++ src/app.js
@@ -2,17 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from the dashboard! brohter again");
+app.use("/user", (req, res) => {
+  res.send("Haha haha hahaha");
 });
 
-app.use("/hello", (req, res) => {
+app.get("/user", (req, res) => {
+  res.send({ name: "Mohammad Zaid", age: 23 });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save Data to the database");
+  res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+// match all http methods
+app.use("/test", (req, res) => {
   res.send("Hello hello hello");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello Hello Hello Hello");
-});
+// app.use("/", (req, res) => {
+//   res.send("Hello from the dashboard!  again  brother");
+// });
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777....");
